3fafea3243a856710c222344d3aafc13e68fb77d
nitesh kumar <niteshgoindani99@gmail.com>
2024-02-13 03:48:18 -0700

    changes in home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     let handleImage = async (image) => {
         let file  = image;
         firebase.imageHandle(file)
-        alert('refresh page')
+        displayImage()
     }
     
     let displayImage = async () => {
